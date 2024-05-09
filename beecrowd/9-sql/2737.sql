e538f0c70dd32826c98713f5d45e30192fcd57c2
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-05-09 12:15:45 -0300

    fix 2737 and 2743

--- beecrowd/9-sql/2737.sql
+++ beecrowd/9-sql/2737.sql
@@ -1,3 +1,12 @@
-SELECT name, LENGTH(name) AS length
-FROM people
-ORDER BY LENGTH(name) DESC;
+(
+    SELECT name, customers_number
+    FROM lawyers
+    WHERE customers_number = (SELECT MAX(customers_number) FROM lawyers) OR
+        customers_number = (SELECT MIN(customers_number) FROM lawyers)
+    ORDER BY customers_number DESC
+)
+UNION ALL
+(
+    SELECT 'Average', AVG(customers_number)::INTEGER
+    FROM lawyers
+);
